d7d16221088113946ccb4f6a13d820f6ab3ea594
LMYMARK <282981106@qq.com>
2014-04-22 11:37:15 +0800

    添加音效，改进动画

--- Classes/WelcomeScene.cpp
+++ Classes/WelcomeScene.cpp
@@ -12,8 +12,8 @@ bool WelcomeScene::init()
 	origin = CCDirector::sharedDirector()->getVisibleOrigin();
 	visibleSize = CCDirector::sharedDirector()->getVisibleSize();
 
-	action_p1 = CCDelayTime::create(0.5f);
-	action_p2 = CCFadeIn::create(1.0f);
+	action_p1 = CCDelayTime::create(1.0f);
+	action_p2 = CCFadeIn::create(1.5f);
 	action_p3 = CCDelayTime::create(3.5f);
 	action_p4 = CCFadeOut::create(0.8f);
 
@@ -31,15 +31,16 @@ void WelcomeScene::playAnimation()
 	project_logo->setOpacity(0);
 	action = CCSequence::create(action_p1,
 		action_p2,
+		CCCallFunc::create(this, callfunc_selector(WelcomeScene::cbShowParticle)),
+		action_p3,
+		action_p1,
 		CCCallFunc::create(this, callfunc_selector(WelcomeScene::cbShowParticle_2)),
-		action_p3,action_p4,NULL);
+		action_p3,
+		action_p3,
+		action_p4,
+		NULL);
 	action->retain();
 	project_logo->runAction(action);
-
-	CocosDenshion::SimpleAudioEngine::sharedEngine()->preloadEffect("24. Mission accomplishment¡¡(LEVEL CLEAR).mp3");
-	CocosDenshion::SimpleAudioEngine::sharedEngine()->playEffect("24. Mission accomplishment¡¡(LEVEL CLEAR).mp3");
-	CocosDenshion::SimpleAudioEngine::sharedEngine()->setEffectsVolume(0.2f);
-
 }
 
 void WelcomeScene::update(float dt)
@@ -48,6 +49,7 @@ void WelcomeScene::update(float dt)
 	{
 		unscheduleUpdate();
 		action->release();
+		CCDirector::sharedDirector()->purgeCachedData();
 		CCDirector::sharedDirector()->replaceScene(HelloWorld::scene());
 	}
 	
@@ -66,34 +68,33 @@ WelcomeScene* WelcomeScene::create()
 }
 void WelcomeScene::cbShowParticle()
 {
-	particle_1 = CCParticleGalaxy::create();
-	particle_2 = CCParticleGalaxy::create();
-	particle_1->setTotalParticles(200);
+	CocosDenshion::SimpleAudioEngine::sharedEngine()->playEffect("¿ª³¡Òô.wav");
+	particle_1 = CCParticleGalaxy::createWithTotalParticles(300);
+	particle_2 = CCParticleGalaxy::createWithTotalParticles(300);
 	particle_1->setLife(2.0f);
-	particle_1->setStartSize(40);
-	particle_1->setEndSize(25);
+	particle_1->setStartSize(60);
+	particle_1->setEndSize(45);
 	particle_1->setSpeed(-25);
 	particle_1->setSpeedVar(4);
 	particle_1->setTangentialAccel(-80);
 	particle_1->setGravity(ccp(0.0f, 0.0f));
-	particle_1->setEmissionRate(200.0f);
+	particle_1->setEmissionRate(300.0 / 2.0);
 	particle_1->setPosition(ccp(origin.x + visibleSize.width / 2 - project_logo->getContentSize().width / 2 - 100,
 		origin.y + visibleSize.height / 2));
-	particle_1->runAction(CCSequence::create(CCMoveBy::create(1.0,ccp(200 + project_logo->getContentSize().width, 0)),
+	particle_1->runAction(CCSequence::create(CCMoveBy::create(1.4f, ccp(200 + project_logo->getContentSize().width, 0)),
 		CCCallFunc::create(this, callfunc_selector(WelcomeScene::cbHideParticle)),
 		NULL));
 
-	particle_2->setTotalParticles(200);
 	particle_2->setLife(2.0f);
-	particle_2->setStartSize(40);
-	particle_2->setEndSize(25);
+	particle_2->setStartSize(60);
+	particle_2->setEndSize(45);
 	particle_2->setSpeed(25);
 	particle_2->setSpeedVar(4);
 	particle_2->setGravity(ccp(0.0f, 0.0f));
-	particle_2->setEmissionRate(200.0f);
+	particle_2->setEmissionRate(300.0 / 2.0);
 	particle_2->setPosition(ccp(origin.x + visibleSize.width / 2 + project_logo->getContentSize().width / 2 + 100,
 		origin.y + visibleSize.height / 2));
-	particle_2->runAction(CCSequence::create(CCMoveBy::create(1.0, ccp(-200 - project_logo->getContentSize().width, 0)),
+	particle_2->runAction(CCSequence::create(CCMoveBy::create(1.4f, ccp(-200 - project_logo->getContentSize().width, 0)),
 		CCCallFunc::create(this, callfunc_selector(WelcomeScene::cbHideParticle)),
 		NULL));
 	addChild(particle_1);
@@ -102,30 +103,34 @@ void WelcomeScene::cbShowParticle()
 
 void WelcomeScene::cbShowParticle_2()
 {
+	CocosDenshion::SimpleAudioEngine::sharedEngine()->playEffect("É­ÁÖ»ð.wav");
+	CocosDenshion::SimpleAudioEngine::sharedEngine()->playEffect("Ä¾²ñ»ð.wav");
 	particle_1 = CCParticleFire::createWithTotalParticles(550);
 	particle_2 = CCParticleFire::createWithTotalParticles(550);
 
 	particle_1->setAngleVar(120);
 	particle_1->setSpeed(45);
-	particle_1->setSpeedVar(10);
+	particle_1->setSpeedVar(20);
 	particle_1->setLife(2.0f);
 	particle_1->setEmissionRate(550.0 / 2.0);
+	particle_1->setGravity(ccp(0, 50));
 	particle_1->setPosition(ccp(origin.x + visibleSize.width / 2 - project_logo->getContentSize().width / 2 - 100,
 		origin.y + visibleSize.height / 2));
-	particle_1->runAction(CCSequence::create(CCMoveBy::create(0.8, ccp(project_logo->getContentSize().width, 0)),
-		CCDelayTime::create(0.8f),
+	particle_1->runAction(CCSequence::create(CCMoveBy::create(0.8f, ccp(project_logo->getContentSize().width, 0)),
+		CCDelayTime::create(2.8f),
 		CCCallFunc::create(this, callfunc_selector(WelcomeScene::cbHideParticle)),
 		NULL));
 
 	particle_2->setAngleVar(120);
 	particle_2->setSpeed(45);
-	particle_2->setSpeedVar(10);
+	particle_2->setSpeedVar(20);
 	particle_2->setLife(2.0f);
 	particle_2->setEmissionRate(550.0 / 2.0);
+	particle_2->setGravity(ccp(0, 50));
 	particle_2->setPosition(ccp(origin.x + visibleSize.width / 2 + project_logo->getContentSize().width / 2 + 100,
 		origin.y + visibleSize.height / 2));
 	particle_2->runAction(CCSequence::create(CCMoveBy::create(0.8, ccp(-project_logo->getContentSize().width, 0)),
-		CCDelayTime::create(0.8f),
+		CCDelayTime::create(2.8f),
 		CCCallFunc::create(this, callfunc_selector(WelcomeScene::cbHideParticle)),
 		NULL));
 	addChild(particle_1);
